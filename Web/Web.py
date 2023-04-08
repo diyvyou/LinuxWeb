538063e525112a0383366dfabbacdda93499219a
JMH <3282995855@qq.com>
2023-04-08 11:11:46 +0800

    日志系统完成

--- Web/Web.py
+++ Web/Web.py
@@ -1,6 +1,9 @@
 # -*- coding: UTF-8 -*-
 from flask import Flask, render_template, send_from_directory, request, redirect, url_for, session
+from flask import request
+from flask import jsonify
 import os
+import time
 
 app = Flask(__name__)
     
@@ -16,17 +19,30 @@ def upload_file():
     file_name = request.form['filename']
     file_ext = file.filename.rsplit('.', 1)[1]
     file.save('/opt/Web/File/' + file_name + '.' + file_ext)
+
+    Time = time.asctime( time.localtime(time.time()) )
+    f = open('/opt/Web/UpLog.log','a+')
+    f.write("\n时间 [{}], IP [{}] , 上传 [{}]".format(Time,request.remote_addr,file_name))
+    f.close()
     return '文件上传成功！'
 
 
 #下载功能
 @app.route('/list')
 def list_files():
+    Time = time.asctime( time.localtime(time.time()) )
+    f = open('/opt/Web/DownLog.log','a+')
+    f.write("\n时间 [{}], IP [{}] 访问".format(Time,request.remote_addr))
+    f.close()
     files = os.listdir('/opt/Web/File')
     return render_template('files.html', files=files)
 
 @app.route('/download/<path:filename>')
 def download_file(filename):
+    Time = time.asctime( time.localtime(time.time()) )
+    f = open('/opt/Web/DownLog.log','a+')
+    f.write("\n时间 [{}], IP [{}] 下载[{}]".format(Time,request.remote_addr,filename))
+    f.close()
     return send_from_directory('/opt/Web/File', filename, as_attachment=True)
 
 
@@ -35,6 +51,12 @@ def download_file(filename):
 def delete_file(filename):
     # 删除 E:\Project\Code\Web 目录下的文件
     os.remove('/opt/Web/File/' + filename)
+
+    Time = time.asctime( time.localtime(time.time()) )
+    f = open('/opt/Web/DownLog.log','a+')
+    f.write("\n时间 [{}], IP [{}] 删除[{}]".format(Time,request.remote_addr,filename))
+    f.close()
+
     return redirect(url_for('list_files'))
 
 
